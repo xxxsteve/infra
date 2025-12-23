92646921887f0072d9ca3417e83f78d002a583c1
wxianxin <wxianxinreg@gmail.com>
2025-12-22 20:48:35 -0500

    fix curl

--- bot/bot_curl.sh
+++ bot/bot_curl.sh
@@ -1,7 +1,7 @@
 # Telegram
 curl -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage \
   -H 'Content-type: application/json' \
-  --data '{"chat_id":"${TELEGRAM_CHAT_ID}","text":"⚚ *ICON TRADING*\n Sharpe ratio over 3000!","parse_mode":"Markdown"}'
+  --data "{\"chat_id\":${TELEGRAM_CHAT_ID},\"text\":\"⚚ *ICON TRADING*\\nSharpe ratio over 3000\\!\",\"parse_mode\":\"Markdown\"}"
 
 # Slack (webhook)
 curl -X POST -H 'Content-type: application/json' \
